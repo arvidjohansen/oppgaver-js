44e8b15ebc3f4f3ed432bc65241cecdd3f040876
arvidjohansen <biskus@gmail.com>
2022-03-21 00:58:34 +0100

    Added opg5

--- opg5/script.js
+++ opg5/script.js
@@ -6,11 +6,25 @@ var formEl = document.getElementById("form");
 var resultEl = document.getElementById("result");
 
 
+
 formEl.addEventListener('submit',function(e){
     e.preventDefault();
     elements = formEl.elements;    
     resultEl.innerHTML = "Hei jeg heter " + elements.navn.value + " og er " + elements.alder.value + " år gammel"
     resultEl.innerHTML += "\n\nJeg liker å drikke " + elements.drikke.value + ".";
+       
+    
+    var mat = []
+    for (var i = 0;i < elements.mat.length;i++){
+        if (elements.mat[i].checked){
+            mat.push(elements.mat[i].value);
+        }
+    }
+    resultEl.innerHTML+="\n\nJeg liker å spise ";
+
+    for (var i = 0;i < mat.length;i++){
+        
+    }
 });
 
 
